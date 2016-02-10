4cf5c7977d07703f68641ec967d78c3a6a13d624
Harrison <harrison@theshoebridges.com>
2016-02-10 18:51:47 +1100

    Use Promise.all to create more idiomatic code

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,45 @@
 import Promise = require('any-promise');
-import extend = require('xtend');
 
 import { readdir, stat, createReadStream } from 'fs';
 import { join } from 'path';
 
-interface doneInterface {
+interface DoneInterface {
   max: number;
   count: number;
   files: string[];
 }
 
-function getFiles(folder: string="./") : Promise<string[]> {
+function getFiles(folder: string='./'): Promise<string[]> {
   return new Promise<string[]>((resolve, reject) => {
     readdir(folder, (err, files) => {
-      let DONE : doneInterface = {
-        max: files.length,
-        count: 0,
-        files: []
-      };
+      let promises: Promise<any>[] = files.map((filename) => {
+        return new Promise<any>((resolve, reject) => {
+          filename = join(folder, filename);
 
-      files.forEach((file, i) => {
-        var file = join(folder, files[i]);
-
-        new Promise<string[]>((resolve, reject) => {
-          stat(file, (err, stats) => {
-            if (err) throw err;
+          stat(filename, (err, stats) => {
+            if (err) {
+              console.log(err);
+              throw err;
+            }
 
             if (stats.isDirectory()) {
-              getFiles(file)
+              getFiles(filename)
               .then((files) => {
                 resolve(files);
               });
             } else {
-              resolve([file]);
+              resolve([filename]);
             }
           });
-        })
-        .then((files) => {
-          let ps : Array<Promise<any>> = files.map((filename) => {
-            return new Promise<any>((resolve, reject) => {
-              DONE.files.push(filename);
-              resolve();
-            });
-          });
-
-          return Promise.all(ps);
-        })
-        .then(() => {
-          DONE.count++;
-          if (DONE.count == DONE.max) {
-            resolve(DONE.files);
-          }
         });
-      })
+      });
+
+      Promise.all(promises)
+      .then(filenames => {
+        // flatten args
+        let files = [].concat.apply([], filenames);
+        resolve(files);
+      });
     });
   });
 }
@@ -65,6 +52,7 @@ export function task(name: string, pipes: string[]) {
       let file = createReadStream(filename);
 
       file.on('data', (chunk : any) => {
+        console.log(`Reading ${filename}:`);
         console.log(chunk.toString());
       });
     });
