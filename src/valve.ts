c35cb9dc164a3e1313efd7de8a7c94642530fca5
Harrison <harrison@theshoebridges.com>
2016-02-12 08:33:44 +1100

    Fix dependencies

--- src/valve.ts
+++ src/valve.ts
@@ -1,10 +1,19 @@
 import { Readable, Writable } from 'stream';
+
 export class Valve {
   public input: any;
   public output: any;
 
   constructor(i: Readable, o: Writable) {
     this.input = i;
+    this.output = o;
+  }
+
+  setInput(i: any) {
+    this.input = i;
+  }
+
+  setOutput(o: any) {
     this.output = o;
   }
 }
