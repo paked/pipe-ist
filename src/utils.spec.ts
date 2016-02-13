174f1b2a297c2793d980e7ca8acca933ca7db5d9
Harrison Shoebridge <harrison@theshoebridges.com>
2016-02-13 22:42:17 +1100

    Fix testing directories

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -5,7 +5,7 @@ import { getFiles } from './utils';
 
 test('utils', t => {
   t.test('list files', t => {
-    const FIXTURE_DIR = join(__dirname, '__test__/file-list');
+    const FIXTURE_DIR = join(__dirname, '__test__/utils-file-list');
 
     return getFiles(FIXTURE_DIR)
       .then(paths => {
