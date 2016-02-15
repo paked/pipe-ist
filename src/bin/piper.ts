5fe410985c61a6d44fe9d261af070be1f02b86bf
Harrison Shoebridge <harrison@theshoebridges.com>
2016-02-15 20:32:56 +1100

    Fix build step

---
+++ src/bin/piper.ts
@@ -0,0 +1,10 @@
+import minimist = require('minimist');
+
+console.log(`
+Usage: piper <task>
+
+'piper' runs the specified task <task> as defined by the 'pipe.js' file.
+
+Options:
+-f      Choose which 'pipe.js' file to run
+`);
